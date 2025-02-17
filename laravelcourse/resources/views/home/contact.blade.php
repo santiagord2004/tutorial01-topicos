6d6e1e9af00030c945b6b531e88589385362d24f
Santiago <santiagord2004>
2025-02-17 17:03:48 -0500

    Progreso hasta actividad 6

---
+++ laravelcourse/resources/views/home/contact.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Contact Us')
+
+@section('content')
+<div class="container">
+    <h1>Contact Us</h1>
+    <p>If you have any questions, please contact us at:</p>
+    <ul>
+        <li>Email: <a href="lionelmessi@gmail.com">lionelmessi@gmail.com</a></li>
+        <li>Address: 123 Bellavista, Colombia</li>
+        <li>Phone: 304 8910 900</li>
+    </ul>
+</div>
+@endsection
